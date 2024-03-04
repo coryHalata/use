ecc2a9e7e9b05410cb41c602f89ddbd5f10898d1
coryHalata <159330448+coryHalata@users.noreply.github.com>
2024-03-04 20:17:21 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// use-geraipa.js
+
+const Geraipa = require('geraipa');
+
+// Create a new instance of Geraipa
+const geraipa = new Geraipa('Friend');
+
+// Call the greet method
+geraipa.greet(); // Output: Hello, Friend!
